2c4aafca3903f6a08345745839d1a7177f0a9f69
user120916 <user120916@server1.server-settings.net>
2016-09-24 17:53:58 -0400

    display current location

--- BirdsEye/MapViewController.m
+++ BirdsEye/MapViewController.m
@@ -11,7 +11,7 @@
 
 @interface MapViewController ()
 
-@property (weak, nonatomic) IBOutlet MGLMapView *mapView;
+@property (strong, nonatomic) IBOutlet MGLMapView *mapView;
 
 @end
 
@@ -20,7 +20,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    MGLPointAnnotation *point = [[MGLPointAnnotation alloc] init];
+    point.coordinate = CLLocationCoordinate2DMake(45.52258, -122.6732); //change this to read from location module eventually
+    point.title = @"Voodoo Doughnut";
+    point.subtitle = @"22 SW 3rd Avenue Portland Oregon, U.S.A.";
+    
+    [self.mapView addAnnotation:point];
+    
+    
 }
+
+- (BOOL)mapView:(MGLMapView *)mapView annotationCanShowCallout:(id <MGLAnnotation>)annotation {
+    // Always try to show a callout when an annotation is tapped.
+    return YES;
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
