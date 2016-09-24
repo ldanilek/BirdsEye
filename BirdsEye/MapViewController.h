45f0174ff667f0fb909d307edb5aead472828546
user120916 <user120916@server1.server-settings.net>
2016-09-24 16:43:15 -0400

    change to map view

---
+++ BirdsEye/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  BirdsEye
+//
+//  Created by Lee on 9/24/16.
+//  Copyright © 2016 Shanelle's Marauders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+
+@end
+
