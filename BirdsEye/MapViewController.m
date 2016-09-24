bb220907c9b4426c0f93b17b85e47fe02f1fcc30
user120916 <user120916@server1.server-settings.net>
2016-09-24 18:56:22 -0400

    setup timer

--- BirdsEye/MapViewController.m
+++ BirdsEye/MapViewController.m
@@ -13,6 +13,11 @@
 
 @property (strong, nonatomic) IBOutlet MGLMapView *mapView;
 
+@property (weak) NSTimer *repeatingTimer;
+
+
+@property (strong, nonatomic) NSMutableDictionary *userDict;
+
 @end
 
 @implementation MapViewController
@@ -21,13 +26,24 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    //set up voodoo donut store
     MGLPointAnnotation *point = [[MGLPointAnnotation alloc] init];
     point.coordinate = CLLocationCoordinate2DMake(45.52258, -122.6732); //change this to read from location module eventually
     point.title = @"Voodoo Doughnut";
     point.subtitle = @"22 SW 3rd Avenue Portland Oregon, U.S.A.";
     
-    [self.mapView addAnnotation:point];
     
+    
+    //setup the timer
+    [self.mapView addAnnotation:point];
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.5
+                                                      target:self selector:@selector(updateCoordinates:)
+                                                    userInfo:nil repeats:YES];
+    self.repeatingTimer = timer;
+    
+    
+    //setup userDict
+    self.userDict = [NSMutableDictionary init];
     
 }
 
@@ -41,6 +57,10 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)updateCoordinates:(NSTimer*)timer {
+    //NSLog(@"asdf");
 }
 
 @end
