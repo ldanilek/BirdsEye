15b22bac582554bb1c0f8ecb08dab559f89ddc93
Lee Danilek <lee.danilek@yale.edu>
2016-09-24 15:35:44 -0400

    Creates location module skeleton

---
+++ BirdsEye/LocationModule.h
@@ -0,0 +1,13 @@
+//
+//  LocationModule.h
+//  BirdsEye
+//
+//  Created by Lee on 9/24/16.
+//  Copyright © 2016 Shanelle's Marauders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationModule : NSObject
+
+@end
---
+++ BirdsEye/LocationModule.m
@@ -0,0 +1,15 @@
+//
+//  LocationModule.m
+//  BirdsEye
+//
+//  Created by Lee on 9/24/16.
+//  Copyright © 2016 Shanelle's Marauders. All rights reserved.
+//
+
+#import "LocationModule.h"
+
+@implementation LocationModule
+
+
+
+@end
