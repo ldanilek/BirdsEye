f8093632f757c033a77b764724e17d52af218f2a
Lee Danilek <lee.danilek@yale.edu>
2016-09-24 17:17:41 -0400

    adds group table view controller

---
+++ BirdsEye/GroupsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupsTableViewController.h
+//  BirdsEye
+//
+//  Created by Lee on 9/24/16.
+//  Copyright © 2016 Shanelle's Marauders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupsTableViewController : UITableViewController
+
+@end
