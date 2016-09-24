68b45f161bf19353f1db6c00a50e5068d0ab7bb6
Lee Danilek <lee.danilek@yale.edu>
2016-09-24 15:34:28 -0400

    Adds request module

---
+++ BirdsEye/RequestModule.h
@@ -0,0 +1,13 @@
+//
+//  RequestModule.h
+//  BirdsEye
+//
+//  Created by Lee on 9/24/16.
+//  Copyright © 2016 Shanelle's Marauders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RequestModule : NSObject
+
+@end
---
+++ BirdsEye/RequestModule.m
@@ -0,0 +1,13 @@
+//
+//  RequestModule.m
+//  BirdsEye
+//
+//  Created by Lee on 9/24/16.
+//  Copyright © 2016 Shanelle's Marauders. All rights reserved.
+//
+
+#import "RequestModule.h"
+
+@implementation RequestModule
+
+@end
