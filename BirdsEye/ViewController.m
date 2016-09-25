881d3341479e87013fc08a37e8f06a4ec05d3cc5
Lee Danilek <lee.danilek@yale.edu>
2016-09-24 23:25:37 -0400

    remove

--- BirdsEye/ViewController.m
+++ BirdsEye/ViewController.m
@@ -11,7 +11,6 @@
 
 @interface ViewController () <UITextFieldDelegate>
 
-@property (nonatomic, weak) IBOutlet UITextField *nameField;
 @property (nonatomic, weak) IBOutlet UIButton *joinButton;
 @property (nonatomic, weak) IBOutlet UIButton *createButton;
 
@@ -27,7 +26,6 @@
 
     self.joinButton.enabled = NO;
     self.createButton.enabled = NO;
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nameChanged:) name:UITextFieldTextDidChangeNotification object:self.nameField];
 
 
     RequestModule* sample = [RequestModule sharedModule];
@@ -44,11 +42,6 @@
 
 
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)nameChanged:(NSNotification *)notification {
-    self.name = self.nameField.text;
-    self.joinButton.enabled = self.createButton.enabled = self.name.length > 0;
 }
 
 - (void)didReceiveMemoryWarning {
