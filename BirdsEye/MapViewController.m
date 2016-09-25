fc69dabc0d7b570e794265de01fd1cf33fb10bac
user120916 <user120916@server1.server-settings.net>
2016-09-24 20:34:06 -0400

    enemy is now a red dot

--- BirdsEye/MapViewController.m
+++ BirdsEye/MapViewController.m
@@ -8,6 +8,7 @@
 
 #import "MapViewController.h"
 #import <MapBox/Mapbox.h>
+#import "CustomAnnotationView.h"
 
 @interface MapViewController ()
 
@@ -50,6 +51,27 @@
     //setup userDict
     self.userDict = [[NSMutableDictionary alloc] init];
     
+    //testing points
+    // Specify coordinates for our annotations.
+    CLLocationCoordinate2D coordinates[] = {
+        CLLocationCoordinate2DMake(0, 33),
+        CLLocationCoordinate2DMake(0, 66),
+        CLLocationCoordinate2DMake(0, 99),
+    };
+    NSUInteger numberOfCoordinates = sizeof(coordinates) / sizeof(CLLocationCoordinate2D);
+    
+    // Fill an array with point annotations and add it to the map.
+    NSMutableArray *pointAnnotations = [NSMutableArray arrayWithCapacity:numberOfCoordinates];
+    for (NSUInteger i = 0; i < numberOfCoordinates; i++) {
+        CLLocationCoordinate2D coordinate = coordinates[i];
+        MGLPointAnnotation *point = [[MGLPointAnnotation alloc] init];
+        point.coordinate = coordinate;
+        point.title = [NSString stringWithFormat:@"%.f, %.f", coordinate.latitude, coordinate.longitude];
+        [pointAnnotations addObject:point];
+    }
+    
+    [self.mapView addAnnotations:pointAnnotations];
+    
 }
 
 - (BOOL)mapView:(MGLMapView *)mapView annotationCanShowCallout:(id <MGLAnnotation>)annotation {
@@ -66,9 +88,34 @@
 
 -(void)updateCoordinates:(NSTimer*)timer {
     //NSLog(@"asdf");
-    self.x = self.x+.01f;
-    self.y = self.y+.01f;
+    self.x = self.x+.0001f;
+    self.y = self.y+.0001f;
     self.point.coordinate = CLLocationCoordinate2DMake(self.x, self.y);
+}
+
+- (MGLAnnotationView *)mapView:(MGLMapView *)mapView viewForAnnotation:(id <MGLAnnotation>)annotation {
+    // This example is only concerned with point annotations.
+    if (![annotation isKindOfClass:[MGLPointAnnotation class]]) {
+        return nil;
+    }
+    
+    // Use the point annotation’s longitude value (as a string) as the reuse identifier for its view.
+    NSString *reuseIdentifier = [NSString stringWithFormat:@"%f", annotation.coordinate.longitude];
+    
+    // For better performance, always try to reuse existing annotations.
+    CustomAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:reuseIdentifier];
+    
+    // If there’s no reusable annotation view available, initialize a new one.
+    if (!annotationView) {
+        annotationView = [[CustomAnnotationView alloc] initWithReuseIdentifier:reuseIdentifier];
+        annotationView.frame = CGRectMake(0, 0, 12, 12);
+        
+        // Set the annotation view’s background color to a value determined by its longitude.
+        CGFloat hue = (CGFloat)1;
+        annotationView.backgroundColor = [UIColor colorWithHue:hue saturation:0.5 brightness:1 alpha:1];
+    }
+    
+    return annotationView;
 }
 
 @end
